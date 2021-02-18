8486f0623c45b163b013e3fb58dff0dd1ad849fa
Manh Ngo <manhnq097@gmail.com>
2021-02-18 17:39:06 +0700

    third commit

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-var canvas = document.createElement('canvas');
-canvas.width = 500;
-canvas.height = 250;
-var ctx = canvas.getContext('2d');
-ctx.font = '30px Cursive';
-ctx.fillText("Hello word!", 50, 50);
-document.querySelector('#App').appendChild(canvas);
+let listWork = [];
+let listWorkElement = document.querySelector('.list-work');
+let addBtnElement = document.querySelector('.action-add a');
+let deleteBtnElement = document.querySelector('.btn-delete');
+document.addEventListener('DOMContentLoaded', () => {
+    if(listWork.length == 0) {
+        listWorkElement.innerText = "Chưa có công việc"
+    }
+    addBtnElement.addEventListener('click', () => {
+        let valueInputElement = document.querySelector('.action-add input').value;
+        if(valueInputElement != '') {
+            listWork.push(valueInputElement);
+            listWorkElement.innerHTML +=
+                `<li>
+                    <label>
+                        <input type="checkbox">
+                        <span>${valueInputElement}</span>
+                    </label>
+                    <a href="javascript:;" class="btn-delete" title="Xoá">Xoá</a>
+                </li>`;
+        console.log('Thêm thành công');
+        }else {
+            console.log('Chưa nhập công việc thì thêm làm sao được?');
+        };
+    });
+});
