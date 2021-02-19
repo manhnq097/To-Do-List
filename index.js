18c7f7beb11be1ae0a23add1d949a13adc2089a9
Manh Ngo <manhnq097@gmail.com>
2021-02-19 11:56:53 +0700

    four commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let listWork = [];
 let listWorkElement = document.querySelector('.list-work');
 let addBtnElement = document.querySelector('.action-add a');
 let deleteBtnElement = document.querySelector('.btn-delete');
+let alertSection = document.querySelector('#alert');
+let status;
 document.addEventListener('DOMContentLoaded', () => {
     if(listWork.length == 0) {
         listWorkElement.innerText = "Chưa có công việc"
@@ -9,6 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
     addBtnElement.addEventListener('click', () => {
         let valueInputElement = document.querySelector('.action-add input').value;
         if(valueInputElement != '') {
+            status = 1;
+            alertFunction();
             listWork.push(valueInputElement);
             listWorkElement.innerHTML +=
                 `<li>
@@ -16,11 +20,31 @@ document.addEventListener('DOMContentLoaded', () => {
                         <input type="checkbox">
                         <span>${valueInputElement}</span>
                     </label>
-                    <a href="javascript:;" class="btn-delete" title="Xoá">Xoá</a>
+                    <a href="javascript:;" class="btn-delete" title="Xoá"><i class="far fa-times-circle"></i></a>
                 </li>`;
-        console.log('Thêm thành công');
         }else {
-            console.log('Chưa nhập công việc thì thêm làm sao được?');
+            status = 0;
+            alertFunction();
         };
     });
 });
+
+function alertFunction () {
+    alertSection.classList.add('active');
+    if (status == 1) {
+        alertSection.innerText = 'Thêm thành công';
+        alertSection.classList.add('success');
+        setTimeout(function() {
+            alertSection.classList.remove('success');
+        }, 1000)
+    }else {
+        alertSection.innerText = 'Thêm thất bại';
+        alertSection.classList.add('fail');
+        setTimeout(function() {
+            alertSection.classList.remove('fail');
+        }, 1000)
+    }
+    setTimeout(function() {
+        alertSection.classList.remove('active');
+    }, 1000)
+}
