5591f8fd8d48c4f85c14f186fbe778eb110b96da
Manh Ngo <manhnq097@gmail.com>
2021-02-26 17:40:32 +0700

    tiep

--- index.js
+++ index.js
@@ -4,24 +4,34 @@ let addBtnElement = document.querySelector('.action-add a');
 let deleteBtnElement = document.querySelector('.btn-delete');
 let alertSection = document.querySelector('#alert');
 let status;
+
 document.addEventListener('DOMContentLoaded', () => {
     addBtnElement.addEventListener('click', () => {
         let valueInputElement = document.querySelector('.action-add input').value;
-        listWork.push(valueInputElement);
-        listWork.map(function(value, index) {
-            listWorkElement.innerHTML +=
-                `<li>
-                    <label>
-                        <input type="checkbox">
-                        <span>${value}</span>
-                    </label>
-                    <a href="javascript:;" class="btn-delete" title="Xoá"><i class="far fa-times-circle"></i></a>
-                </li>`;
-        });
+        if(valueInputElement != '') {
+            listWork.push(valueInputElement);
+            console.log(listWork);
+            listWorkElement.innerHTML = '';
+            listWork.map(function(v) {
+                listWorkElement.innerHTML +=
+                    `<li>
+                        <label>
+                            <input type="checkbox">
+                            <span>${v}</span>
+                        </label>
+                        <a href="javascript:;" class="btn-delete" title="Xoá" onClick="removeItem(this)"><i class="far fa-times-circle"></i></a>
+                    </li>`;
+            });
+            status = 1;
+            alertFunction();
+        }else {
+            status = 2;
+            alertFunction();
+        }
     });
 });
 
-const alertFunction = () => {
+function alertFunction() {
     alertSection.classList.add('active');
     if (status == 1) {
         alertSection.innerText = 'Thêm thành công!';
@@ -39,4 +49,8 @@ const alertFunction = () => {
     setTimeout(function() {
         alertSection.classList.remove('active');
     }, 1000)
+}
+
+function removeItem(e) {
+    console.log(e);
 }
