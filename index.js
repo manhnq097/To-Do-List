8815a668d5fbc2d0c27805cd5dbc4011157c749e
Manh Ngo <47490390+manhnq097@users.noreply.github.com>
2021-03-04 08:27:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let listWork = [];
+let objectNew = {};
 let listWorkElement = document.querySelector('.list-work');
 let addBtnElement = document.querySelector('.action-add a');
 let deleteBtnElement = document.querySelector('.btn-delete');
@@ -7,40 +8,65 @@ let status;
 
 document.addEventListener('DOMContentLoaded', () => {
     addBtnElement.addEventListener('click', () => {
-        let valueInputElement = document.querySelector('.action-add input').value;
-        if(valueInputElement != '') {
-            listWork.push(valueInputElement);
-            console.log(listWork);
-            listWorkElement.innerHTML = '';
-            listWork.map(function(v) {
-                listWorkElement.innerHTML +=
-                    `<li>
-                        <label>
-                            <input type="checkbox">
-                            <span>${v}</span>
-                        </label>
-                        <a href="javascript:;" class="btn-delete" title="Xoá" onClick="removeItem(this)"><i class="far fa-times-circle"></i></a>
-                    </li>`;
-            });
-            status = 1;
-            alertFunction();
-        }else {
-            status = 2;
-            alertFunction();
-        }
+        addItem();
+        renderItem();
+        console.clear();
+        console.log(listWork);
     });
 });
+
+function addItem() {
+    let valueInputElement = document.querySelector('.action-add input').value;
+    if(valueInputElement != '') {
+        objectNew = {};
+        objectNew.id = randomId();
+        objectNew.title = valueInputElement;
+        listWork.push(objectNew);
+        status = 1;
+        alertFunction();
+    }else {
+        status = 2;
+        alertFunction();
+    }
+};
+
+function removeItem(id) {
+    const index = listWork.indexOf(id);
+    listWork.splice(index, 1);
+    renderItem();
+};
+
+function renderItem() {
+    listWorkElement.innerHTML = '';
+    listWork.map(function(v) {
+        listWorkElement.innerHTML +=
+            `<li id="${v.id}">
+                <label>
+                    <input type="checkbox">
+                    <span>${v.title}</span>
+                </label>
+                <a href="javascript:;" class="btn-delete" title="Xoá" onClick="removeItem('${v.id}')"><i class="fas fa-trash"></i></a>
+            </li>`;
+    });
+};
+
+function randomId() {
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+};
 
 function alertFunction() {
     alertSection.classList.add('active');
     if (status == 1) {
-        alertSection.innerText = 'Thêm thành công!';
+        alertSection.innerText = 'Success!';
         alertSection.classList.add('success');
         setTimeout(function() {
             alertSection.classList.remove('success');
         }, 1000)
     }else {
-        alertSection.innerText = 'Thêm thất bại!';
+        alertSection.innerText = 'Fail!';
         alertSection.classList.add('fail');
         setTimeout(function() {
             alertSection.classList.remove('fail');
@@ -49,8 +75,4 @@ function alertFunction() {
     setTimeout(function() {
         alertSection.classList.remove('active');
     }, 1000)
-}
-
-function removeItem(e) {
-    console.log(e);
-}
+};
