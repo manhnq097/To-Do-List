b1334409d0022ef8b47e4fc6620e996a7784712a
Manh Ngo <manhnq097@gmail.com>
2021-03-16 16:41:48 +0700

    test

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ let listWork = [];
 let objectNew = {};
 let listWorkElement = document.querySelector('.list-work');
 let deleteBtnElement = document.querySelector('.btn-delete');
+const form = document.forms["formWork"];
 
-
+const getDate = () => {
+    let dateObj = new Date();
+    var month = ('0' + (dateObj.getMonth() + 1)).slice(-2);
+    var date = ('0' + dateObj.getDate()).slice(-2);
+    var year = dateObj.getFullYear();
+    return `Ngày ${date} tháng ${month} năm ${year}`;
+}
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.forms["formWork"];
+    let heading = document.querySelector('#app .heading h1');
+    heading.innerText = getDate();
     renderItem();
     form.addEventListener('submit', function(e) {
         e.preventDefault();
@@ -13,18 +21,24 @@ document.addEventListener('DOMContentLoaded', () => {
         renderItem();
         console.clear();
         console.log(listWork);
-        form.reset();
     });
 });
 
 const addItem = () => {
-    let titleWork = document.querySelector('.action-add input').value;
-    if(titleWork != '') {
+    let title = document.querySelector('form input[name="title"]').value;
+    let brief = document.querySelector('form input[name="brief"]').value;
+    let startTime = document.querySelector('form input[name="startTime"]').value;
+    let endTime = document.querySelector('form input[name="endTime"]').value;
+    if(title != '') {
         objectNew = {};
         objectNew.id = randomId();
-        objectNew.title = titleWork;
+        objectNew.title = title;
+        objectNew.brief = brief;
+        objectNew.startTime = startTime;
+        objectNew.endTime = endTime;
         listWork.push(objectNew);
         mNotification.success('Thêm thành công');
+        form.reset();
     }else {
         mNotification.error('Bạn chưa nhập công việc');
     }
@@ -37,34 +51,23 @@ const removeItem = (id) => {
     mNotification.success('Xoá thành công');
 };
 
-const clearAll = () => {
-    listWork = [];
-    renderItem();
-    mNotification.success('Xoá tất cả thành công');
-}
-
 const renderItem = () => {
     if(listWork.length > 0) {
         listWorkElement.innerHTML = '';
-        listWork.map(function(v) {
+        listWork.map(function(v, i) {
             listWorkElement.innerHTML +=
-                `<tr id="${v.id}">
-                    <td style="text-align: center;">
-                        <input type="checkbox">
-                    </td>
-                    <td>
-                        <span>${v.title}</span>
-                    </td>
-                    <td>
-                        <span>${v.title}</span>
-                    </td>
-                    <td style="text-align: center;">
-                        <a href="javascript:;" class="btn-delete" title="Xoá" onClick="removeItem('${v.id}')"><i class="fas fa-trash"></i></a>
-                    </td>
-                </tr>`;
+                `<li id="${v.id}" class="item">
+                    <div class="time"><span class="item-title">Từ:&nbsp;</span><span class="item-content">${v.startTime}</span>&nbsp;-&nbsp;<span class="item-title">đến:&nbsp;</span><span class="item-content">${v.endTime}</span></div>
+                    <div><span class="item-title">Công việc:&nbsp;</span><span class="item-content">${v.title}</span></div>
+                    <div><span class="item-title">Mô tả công việc:&nbsp;</span><span class="item-content">${v.brief}</span></div>
+                    <div>
+                        <a href="javascript:;" class="btn btn-sm btn-success" title="Đã xong"><i class="fas fa-check"></i></a>
+                        <a href="javascript:;" class="btn btn-sm btn-danger" title="Xoá" onClick="removeItem('${v.id}')"><i class="fas fa-trash"></i></a>
+                    </div>
+                </li>`;
         });
     }else {
-        listWorkElement.innerHTML = `<tr style="text-align: center;"><td colspan="5">Chưa có công việc nào</td></tr>`
+        listWorkElement.innerHTML = `<div style="text-align: center;">Chưa có công việc nào</div>`
     }
     
 };
